cc585e09e28ffffe9f95a17b9c06fa9a6990ef01
ashofbarkhia <40876861+ashofbarkhia@users.noreply.github.com>
2019-11-28 17:24:01 +0700

    Add files via upload

---
+++ application/views/user/pengurus/perizinan.php
@@ -0,0 +1,35 @@
+<?php
+?>
+<html>
+<head>
+ <title>Struktur Table</title>
+</head>
+<body>
+<table border="1" width="300">
+ <caption>DAFTAR NAMA WARGA YANG IZIN</caption>
+ <thead>
+  <tr>
+   <td>id izin</td>
+   <td>id warga</td>
+   <td>tanggal mulai</td>
+   <td>tanggal kembali</td>
+   <td>keterangan izin</td>
+   <td>id musahil</td>
+   <td>id pengurus</td>
+  </tr>
+ </thead>
+ <tbody>
+  <tr>
+   <td>1</td>
+   <td>bacacoding</td>
+   <td>Laki-Laki</td>
+  </tr>
+  <tr>
+   <td>2</td>
+   <td>Cewek Manis</td>
+   <td>Perempuan</td>
+  </tr>
+ </tbody>
+</table>
+</body>
+</html>
