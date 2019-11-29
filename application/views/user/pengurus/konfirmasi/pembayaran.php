2cfd55bc1c0bffc847e33dde0f2f58adfcb301e6
4kari <45159811+4kari@users.noreply.github.com>
2019-11-29 21:33:40 +0700

    Update pembayaran.php

--- application/views/user/pengurus/konfirmasi/pembayaran.php
+++ application/views/user/pengurus/konfirmasi/pembayaran.php
@@ -1,13 +1,7 @@
 <?php
 	$this->load->view('Temp/pengurus/header');
+    $pendaf=$this->db->get('pendaftaran')->result();
 ?>
-<div id="page-content-wrapper">
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-lg-12"></div>
-		</div>
-	</div>
-</div>
 <div class="page-container">
     <!-- MAIN CONTENT-->
     <div class="main-content">
@@ -15,92 +9,51 @@
             <div class="container-fluid">
 				<div class="row">
                     <div class="col-lg-9">
-                        <h2 class="title-1 m-b-25">Data Pembayaran</h2>
+                        <h2 class="title-1 m-b-25">Data pendaftaran</h2>
                         <div class="table-responsive table--no-card m-b-40">
                             <table class="table table-borderless table-striped table-earning">
                                 <thead>
-                                    <tr>
-                                        <th>date</th>
-                                        <th>order ID</th>
-                                        <th>name</th>
-                                        <th class="text-right">price</th>
-                                        <th class="text-right">quantity</th>
-                                        <th class="text-right">total</th>
-                                    </tr>
+                                    <?php
+                                        echo "<tr>";
+                                            echo "<th> Nama </th>";
+                                            echo "<th> NIM </th>";
+                                            echo "<th> Prodi </th>";
+                                            echo "<th> Alamat </th>";
+                                            echo "<th> Jenis Kelamin </th>";
+                                            echo "<th> Action </th>";
+                                        echo "</tr>";
+                                    ?>
+                                    
                                 </thead>
                                 <tbody>
-                                    <tr>
-                                        <td>2018-09-29 05:57</td>
-                                        <td>100398</td>
-                                        <td>iPhone X 64Gb Grey</td>
-                                        <td class="text-right">$999.00</td>
-                                        <td class="text-right">1</td>
-                                        <td class="text-right">$999.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2018-09-28 01:22</td>
-                                        <td>100397</td>
-                                        <td>Samsung S8 Black</td>
-                                        <td class="text-right">$756.00</td>
-                                        <td class="text-right">1</td>
-                                        <td class="text-right">$756.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2018-09-27 02:12</td>
-                                        <td>100396</td>
-                                        <td>Game Console Controller</td>
-                                        <td class="text-right">$22.00</td>
-                                        <td class="text-right">2</td>
-                                        <td class="text-right">$44.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2018-09-26 23:06</td>
-                                        <td>100395</td>
-                                        <td>iPhone X 256Gb Black</td>
-                                        <td class="text-right">$1199.00</td>
-                                        <td class="text-right">1</td>
-                                        <td class="text-right">$1199.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2018-09-25 19:03</td>
-                                        <td>100393</td>
-                                        <td>USB 3.0 Cable</td>
-                                        <td class="text-right">$10.00</td>
-                                        <td class="text-right">3</td>
-                                        <td class="text-right">$30.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2018-09-29 05:57</td>
-                                        <td>100392</td>
-                                        <td>Smartwatch 4.0 LTE Wifi</td>
-                                        <td class="text-right">$199.00</td>
-                                        <td class="text-right">6</td>
-                                        <td class="text-right">$1494.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2018-09-24 19:10</td>
-                                        <td>100391</td>
-                                        <td>Camera C430W 4k</td>
-                                        <td class="text-right">$699.00</td>
-                                        <td class="text-right">1</td>
-                                        <td class="text-right">$699.00</td>
-                                    </tr>
-                                    <tr>
-                                        <td>2018-09-22 00:43</td>
-                                        <td>100393</td>
-                                        <td>USB 3.0 Cable</td>
-                                        <td class="text-right">$10.00</td>
-                                        <td class="text-right">3</td>
-                                        <td class="text-right">$30.00</td>
-                                    </tr>
+                                    <?php
+                                    foreach ($pendaf as $row) {
+                                        if($row->keterangan!="Sudah di verivikasi silahkan buat akun"){
+                                            $jk=$this->db->get_where('jk',["id_jk" => $row->id_jk])->row();
+                                            $id=$row->id_daftar;
+                                            echo "<tr>";
+                                                echo "<td> $row->namalengkap</td>";
+                                                echo "<td> $row->nim</td>";
+                                                echo "<td> $row->prodi</td>";
+                                                echo "<td> $row->alamat</td>";
+                                                echo "<td> $jk->jk</td>";?>
+                                                <td> <a href="<?php echo site_url('pengurus/proses_pendaftaran/'); echo($id); ?>" class="btn btn-success">Lihat</a></td>
+                                            <?php
+                                            echo "</tr>";
+                                        }
+                                    }
+                                    ?>
                                 </tbody>
                             </table>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+        <!-- END MAIN CONTENT-->
+        <!-- END PAGE CONTAINER-->
     </div>
-    <!-- END MAIN CONTENT-->
-    <!-- END PAGE CONTAINER-->
 </div>
+</body>
+</html>
