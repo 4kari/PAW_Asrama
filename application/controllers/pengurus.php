58c849492613c25ab06336c668039417cd0cbdd0
4kari <45159811+4kari@users.noreply.github.com>
2019-11-29 17:50:53 +0700

    Update pengurus.php

--- application/controllers/pengurus.php
+++ application/controllers/pengurus.php
@@ -12,16 +12,43 @@ class pengurus extends CI_Controller {
 		$this->load->view("user/pengurus/index");
 	}
 	public function pendaftaran(){
-		$this->load->view("user/pengurus/pendaftaran");
+		$this->load->view("user/pengurus/konfirmasi/pendaftaran");
 	}
 	public function pembayaran(){
-		$this->load->view("user/pengurus/pembayaran");
+		$this->load->view("user/pengurus/konfirmasi/pembayaran");
 	}
 	public function kegiatan(){
-		$this->load->view("user/pengurus/kegiatan");
+		$this->load->view("user/pengurus/konfirmasi/kegiatan");
+	}
+	public function perizinan(){
+		$this->load->view('user/pengurus/konfirmasi/perizinan');
 	}
 	public function show_pendaftaran($id){
 		$data['id']=$id;
-		$this->load->view("user/pengurus/show_pendaftaran", $data);
+		$this->load->view("user/pengurus/konfirmasi/show_pendaftaran", $data);
+	}
+	public function warga(){
+		$this->load->view('user/pengurus/data/warga');
+	}
+	public function musahil(){
+		$this->load->view('user/pengurus/data/musahil');
+	}
+	public function pelanggaran(){
+		$this->load->view('user/pengurus/data/pelanggaran');
+	}
+	public function kegiatan2(){
+		$this->load->view('user/pengurus/data/kegiatan');
+	}
+	public function izin(){
+		$this->load->view('user/pengurus/data/izin');
+	}
+	public function account(){
+		$this->load->view('user/pengurus/account');
+	}
+	public function settings(){
+		$this->load->view('user/pengurus/settings');
+	}
+	public function logout(){
+		$this->load->view('user/pengurus/logout');
 	}
 }
