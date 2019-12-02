af5bd92645543f61f98cc72606e3a4e8b2a9a533
yuniapuspita <58286947+yuniapuspita@users.noreply.github.com>
2019-12-02 20:28:09 +0700

    Add files via upload

---
+++ application/views/informasi.php
@@ -0,0 +1,70 @@
+<?php
+    $_SESSION['page']='utama';
+    $this->load->view('Temp/header');
+    $this->load->view('Temp/mlog');
+?>
+<!--- info 1--->
+<div id="info" class="offset">
+  <div class="col-12 narrow  p-5 bg-light">
+    <h1>PENDAFTARAN WARGA BARU (MABA) ASRAMA MAHASISWA 2020</h1>
+    <p class="lead">Diumumkan bahwa Asrama Mahasiswa Universitas Trunojoyo Membuka pendaftaran Warga Baru Asrama periode 2015-2016 bagi Mahasiswa Baru UTM angkatan 2015 dengan waktu dan Ketentuan sebagai berikut:</p>
+    <p class="lead">A. WAKTU PENDAFTARAN</p>
+    <p class="lead">1.  Gelombang 1 : 9 Juni 2015 – 11 Juni 2019</p>
+    <p class="lead">2.  Gelombang 2 : Sesuai Jadwal Daftar Ulang SBMPTN Tulis</p>
+    <p class="lead">B. KUALIFIKASI UMUM</p>
+    <p class="lead">1.  Mahasiswa Baru Universitas Trunojoyo angkatan 2014.</p>
+    <p class="lead">3.  Tidak Memiliki Penyakit bawaan yang berpotensi menular dan atau kronis (sesuai daftar penyakit kronis atau menular dari ikatan dokter dan dinas kesehatan).</p>
+    <p class="lead">C. PERSYARATAN PENDAFTARAN DAN ADMINISTRASI</p>
+    <p class="lead">Proses pendaftaran wajib dilakukan dalam dua tahap, yang diatur sebagai berikut:</p>
+    <p class="lead">C1. Pendaftaran ONLINE</p>
+    <p class="lead">1.  Mengisi form biodata dengan klik disini Form Biodata Pendaftaran Asrama</p>
+    <p class="lead">2.  Pelaksanaan Pendaftaran Online dilakukan sebelum pendaftaran fisik sesuai jadwal gelombang 1 dan 2 di point A.</p>
+    <p class="lead">C2. Pendaftaran Dokumen FISIK</p>
+    <p class="lead">1.  Membayar uang pendaftaran sebesar Rp. 50.000,-</p>
+    <p class="lead">2.  Membayar uang kegiatan pendidikan karakter asrama selama 6 bulan didepan </p>
+    <p class="lead">total sebesar Rp. 900.000,- (@Rp. 150.000,-/bulan) atau dapat juga dilakukan </p>
+    <p class="lead">Transfer melalui rekening </p>
+    <p class="lead">o BANK JATIM </p>
+    <p class="lead">o No Rek: 0251009099 </p>
+    <p class="lead">o Atas Nama : Rektor UTM</p>
+    <p class="lead">3.  Menyerahkan berkas-berkas fisik (cetak) berupa: </p>
+    <p class="lead">o Formulir Biodata(sesuai Format) Biodata Warga Asrama v0.3</p>
+    <p class="lead">o Surat Pernyataan Kesanggupan Mematuhi Aturan Asrama(sesuai Format) Checklist n Pernyataan Warga Asrama v0.1</p>
+    <p class="lead">o Pas Foto berwarna 4×6 sebanyak 2 lembar</p>
+    <p class="lead">o FC Kartu Tanda Penduduk (KTP)</p>
+    <p class="lead">o FC Kartu SusunanKeluarga (KSK)</p>
+    <p class="lead">o FC Kartu TandaPenduduk (KTP) Orang Tua / Wali sesuai KSK</p>
+    <p class="lead">o FC Sertifikat Kegiatan atau Prestasi (jika ada)</p>
+    <p class="lead">o Kwitansi Asli Pendaftaran (saat Check in)</p>
+    <p class="lead">o Kwitansi Pembayaran Asrama atau Bukti Transfer ke Rekening Rektor (saat Check in)</p>
+    <p class="lead">4.  Berkas-berkas pendaftaran dimasukkan dalam map dengan ketentuan warna Biru bagi putra/mahasiswa, dan Hijau bagi putri/mahasiswi</p>
+    <p class="lead">5.  Pelaksanaan Daftar Ulang (Fisik) sesuai jadwal pendaftaran gelombang 1 dan 2 di point A</p>
+    <p class="lead">6.  Pendaftar wajib mengirim seluruh berkas cetak pendaftaran (file dan hasil scan) ke email asrama: asrama.unijoyo@gmail.com selambatlambatnya H+3 setelah pendaftaran. File dan scan berkas cetak dijadikan satu file dengan format rar/ zip. </p>
+    <p class="lead">Format penamaan rar/ zip dan subjek email: </p>
+    <p class="lead">o Mahasiswa : Asrama Putra4_NIM_Nama Lengkap_2015/2016</p>
+    <p class="lead">o Mahasiswi : Asrama Putri4_NIM_Nama Lengkap_2015/2016</p>
+    <p class="lead">7.  Tempat Pendaftaran </p>
+    <p class="lead">o  Untuk Mahasiswa di Gedung D Asrama Putra</p>
+    <p class="lead">o  Untuk Mahasiswi di Gedung A Asrama Putri</p>
+    <p class="lead">D. LAIN LAIN</p>
+    <p class="lead">1.  Contact Person:</p>
+    <p class="lead">o Mondzir  (Ketupel Pendaftaran Putra) : 087 750 181 332</p>
+    <p class="lead">o Himmatul Aliyah (Ketupel Pendaftaran  Putri) : 085 745 224 426</p>
+    <p class="lead">2.  Informasi Selengkapnya :  asrama.trunojoyo.ac.id</p>
+    <p class="lead">Email Asrama UTM :  asrama.unijoyo@gmail.com</p>
+    <p class="lead">3.  Daftar warga yang diterima di asrama akan diumumkan pada tanggal 8 Agustus 2015.</p>
+    <p class="lead">Bangkalan, 1 Juni 2015</p>
+    <p class="lead">Ketua Asrama Mahasiswa</p>
+    <p class="lead">Universitas Trunojoyo Madura</p>
+    <p class="lead"></p>
+    <p class="lead">ttd</p>
+    <p class="lead"></p>
+    <p class="lead">Ahmad Agus Ramdlany, SH., MH</p>
+    <p class="lead">197908212003121002</p>
+    <p class="lead"></p>
+    <a class="btn btn-secondary btn-md" href="">See More</a>
+  </div>
+</div>
+<div class="fixed narrow">&nbsp;</div>
+</body>
+</html>
