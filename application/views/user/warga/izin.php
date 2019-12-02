a1fa40b370f6ab3174a5728f4bdf8831991e2bb9
yuniapuspita <58286947+yuniapuspita@users.noreply.github.com>
2019-12-02 20:39:02 +0700

    Update izin.php

--- application/views/user/warga/izin.php
+++ application/views/user/warga/izin.php
@@ -4,7 +4,7 @@
 <html>
 <head>
 	<title>Beranda-Warga Asrama</title>
-  <link rel="stylesheet" type="text/css" href="<?= base_url(); ?>css/style.css">
+  	<link rel="stylesheet" type="text/css" href="<?= base_url(); ?>css/style.css">
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
