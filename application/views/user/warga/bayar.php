e34ab57b4695e4c05e28927eeefca14c8b434469
yuniapuspita <58286947+yuniapuspita@users.noreply.github.com>
2019-12-05 11:07:24 +0700

    Update bayar.php

--- application/views/user/warga/bayar.php
+++ application/views/user/warga/bayar.php
@@ -32,9 +32,6 @@
         </a>
         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
           <a class="dropdown-item" href="<?php echo site_url('izin') ?>">Izin</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="<?php echo site_url('') ?>">Pindah Kamar</a>
-          <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="<?php echo site_url('izin/bayar') ?>">Pembayaran</a>
           <div class="dropdown-divider"></div>
           <a class="dropdown-item" href="<?php echo site_url('') ?>">Informasi Pembayaran</a>
